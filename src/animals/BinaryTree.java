9d51474508167ac1f46a0bbde97ab2483d064f76
Denis Stoyanouski <vandenst@tut.by>
2022-09-14 20:41:33 +0300

    added class BinaryTree;

---
+++ src/animals/BinaryTree.java
@@ -0,0 +1,15 @@
+package animals;
+
+public class BinaryTree {
+    // Root of Binary Tree
+    Node root;
+
+    // Constructors
+    BinaryTree(String key) {
+        root = new Node(key);
+    }
+
+    BinaryTree() {
+        root = null;
+    }
+}
